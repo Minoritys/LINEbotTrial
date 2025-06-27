ed481b868875b10d7728726d364545106a9ee90d
Minoritys <rx78.2.mn@gmail.com>
2025-06-28 02:38:21 +0900

    レスポンスにかかった時間を出力するように変更

--- main.js
+++ main.js
@@ -25,9 +25,16 @@ function main() {
     payload: JSON.stringify(payload),
   };
 
+  const startTime = new Date().getTime();
   const response = UrlFetchApp.fetch(url, options);
+  const endTime = new Date().getTime();
+  const responseTimeMs = endTime - startTime;
+  const responseTimeSec = (responseTimeMs / 1000).toFixed(2);
+  
   const data = JSON.parse(response);
   const content = data["candidates"][0]["content"]["parts"][0]["text"];
+  
+  console.log(`レスポンス時間: ${responseTimeSec} 秒`);
   console.log(data);
   console.log(content);
 }
