0d329ec93fceaf6113f5fafb525ea534f2e23bfd
Minoritys <rx78.2.mn@gmail.com>
2025-07-02 00:39:12 +0900

    2.0 flash lite では実行されなかったのでモデルを変更

--- gemini.js
+++ gemini.js
@@ -37,7 +37,7 @@ function callGeminiAPI(prompt, userId) {
     ],
   };
 
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=${GEMINI_API_KEY}`;
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_API_KEY}`;
   const options = {
     method: "POST",
     contentType: "application/json",
@@ -137,4 +137,12 @@ function clearConversationHistory(userId, prompt, cache) {
   cache.put("history", JSON.stringify(newHistoryJson));
   modelResponse = callGeminiAPI("", userId);
   return modelResponse;
+}
+
+function test() {
+  const cache = new customCache("test");
+  const text = "会話履歴を消してください";
+  const response = callGeminiAPI(text, "test");
+  log.log(response);
+  cache.remove("history");
 }
