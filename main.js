72d18205874e878afb7ed0aab01784fac8e6703e
Minoritys <rx78.2.mn@gmail.com>
2025-07-01 12:33:26 +0900

    LINE応答機能作成

---
+++ main.js
@@ -0,0 +1,43 @@
+const LINE_API_KEY =
+  PropertiesService.getScriptProperties().getProperty("LINE_API_KEY");
+
+function doPost(e) {
+  const events = JSON.parse(e.postData.contents).events;
+
+  // https://developers.line.biz/ja/reference/messaging-api/#message-event
+  for (const event of events) {
+    const reply_token = event.replyToken;
+    const eventType = event.type;
+    const messageText = event.message.text;
+    const replyText = callGeminiAPI(messageText);
+
+    if (!reply_token) {
+      continue;
+    }
+    if (eventType == "message") sendReplyMessage(replyText, reply_token);
+  }
+  return;
+}
+
+function sendReplyMessage(messageText, reply_token) {
+  const option = {
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+      Authorization: "Bearer " + LINE_API_KEY,
+    },
+    method: "post",
+    payload: JSON.stringify({
+      replyToken: reply_token,
+      messages: [
+        {
+          type: "text",
+          text: messageText,
+        },
+      ],
+    }),
+  };
+
+  UrlFetchApp.fetch("https://api.line.me/v2/bot/message/reply", option);
+
+  return;
+}
