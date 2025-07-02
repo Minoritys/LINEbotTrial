f8558f93fe24fe33cf8db9126a8fdc8633041743
Minoritys <rx78.2.mn@gmail.com>
2025-07-02 10:10:00 +0900

    Loggerにシートへの書き込み機能を追加

--- utils.js
+++ utils.js
@@ -1,3 +1,6 @@
+const SPREADSHEET_ID =
+  PropertiesService.getScriptProperties().getProperty("SPREADSHEET_ID");
+
 // GASのcache service をカスタムしてユーザーごとにわける
 // https://note.com/luth/n/nbc1e20d82c3f
 class customCache {
@@ -26,10 +29,29 @@ class customCache {
 class customLogger {
   constructor() {
     this.logs = [];
+    this.spreadsheet = SpreadsheetApp.openById(SPREADSHEET_ID);
+    this.errorSheet = this.spreadsheet.getSheetByName("errorLog");
+    this.logSheet = this.spreadsheet.getSheetByName("log");
   }
   log(arg) {
     console.log(arg);
-    //this.logs.push(arg);
+    this.logs.push([new Date(), arg]);
+  }
+  error(arg) {
+    console.error(arg);
+    this.errorSheet.appendRow([new Date(), arg]);
+  }
+  writeLogSheet(userId) {
+    this.logs.map((log) => {
+      log.push(userId);
+    });
+    const lastRow = this.logSheet.getLastRow();
+    const rowLength = this.logs.length;
+    const columnLength = this.logs[0].length;
+    this.logSheet
+      .getRange(lastRow + 1, 1, rowLength, columnLength)
+      .setValues(this.logs);
+    this.logs = [];
   }
 }
 const log = new customLogger();
