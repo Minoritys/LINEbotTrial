5a63612143967d08bece63b44075c008eb7a6156
Minoritys <rx78.2.mn@gmail.com>
2025-07-02 11:23:16 +0900

    geminiに渡す関数の定義

--- prompt.js
+++ prompt.js
@@ -1,2 +1,22 @@
 const systemInstructionText = `- 必ず日本語で回答してください\n- 回答にマークダウン記法を使用しないでください`;
+const functionDefinitions = [
+  {
+    name: "clearConversationHistory",
+    description: "ここまでの会話履歴を消去します",
+    parameters: {
+      type: "object",
+      properties: {},
+      required: [],
+    },
+  },
+  {
+    name: "getWeatherOfTokyo",
+    description: "東京の天気を取得します",
+    parameters: {
+      type: "object",
+      properties: {},
+      required: [],
+    },
+  },
+];
 const inputText = `API呼び出しのテストをしています。この文章はとどいていますか？`;
